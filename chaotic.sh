01d9628fa66435fed8c4ec68bc917799634355ae
Matthew B. <131808292+Crowdrocker@users.noreply.github.com>
2025-08-18 18:52:28 -0500

    Update chaotic.sh

--- chaotic.sh
+++ chaotic.sh
@@ -31,7 +31,7 @@ Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 fi
 
 ### cachyos-repo
-wget https://mirror.cachyos.org/cachyos-repo.tar.xz
+curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz && cd cachyos-repo
 sudo ./cachyos-repo.sh
 
